74236704f5207c8a20295bd068f585a187c9c7f6
Jonathan Dick <jondick@gmail.com>
2021-04-30 23:26:29 -0400

    Fix windows native features

--- BlazorWeather.Maui.WinUI3/Windows/NotificationService.Windows.cs
+++ BlazorWeather.Maui.WinUI3/Windows/NotificationService.Windows.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WeatherTwentyOne.Services;
+using Microsoft.Maui;
 
 namespace BlazorWeather.Maui.Windows
 {
--- BlazorWeather.Maui.WinUI3/Windows/TrayService.Windows.cs
+++ BlazorWeather.Maui.WinUI3/Windows/TrayService.Windows.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WeatherTwentyOne.Services;
+using BlazorWeather.Maui;
 
 namespace BlazorWeather.Maui.Windows
 {
