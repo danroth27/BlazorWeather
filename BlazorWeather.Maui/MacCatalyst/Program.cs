941951abbe9eaa6997f713fa79da9cc331f6ac2f
Jonathan Dick <jond@hey.com>
2021-05-03 11:51:16 -0400

    Add explicit native http handler switch

--- BlazorWeather.Maui/MacCatalyst/Program.cs
+++ BlazorWeather.Maui/MacCatalyst/Program.cs
@@ -7,6 +7,8 @@ namespace BlazorWeather.Maui
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
+			System.AppContext.SetSwitch("System.Net.Http.UseNativeHttpHandler", true);
+
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main(args, null, "AppDelegate");
