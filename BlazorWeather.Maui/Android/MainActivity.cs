f747e29238079b808e4efb8a26fb561caa058410
Daniel Roth <daroth@microsoft.com>
2021-05-04 09:19:49 -0700

    Add Android folder

---
+++ BlazorWeather.Maui/Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Microsoft.Maui;
+
+namespace BlazorWeather.Maui
+{
+	[Activity(Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+	}
+}
---
+++ BlazorWeather.Maui/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Microsoft.Maui;
+
+namespace BlazorWeather.Maui
+{
+	[Application]
+	public class MainApplication : MauiApplication<Startup>
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownerShip) : base(handle, ownerShip)
+		{
+		}
+	}
+}
